08a2a37b2687df4788437511269dec4170a198a9
ellaverak <ella.korkeaaho@gmail.com>
2022-03-28 14:36:51 +0300

    modify laskarit/viikko2/unicafe/src/tests/maksukortti_test.p

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -8,23 +8,23 @@ class TestMaksukortti(unittest.TestCase):
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
 
-    def kortin_saldo_alussa_oikein(self):
-        self.assertEqual(str(self.maksukortti), "saldo: 10")
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
 
-    def rahan_lataaminen_toimii_oikein(self):
+    def test_rahan_lataaminen_toimii_oikein(self):
         self.maksukortti.lataa_rahaa(10)
-        self.assertEqual(str(self.maksukortti), "saldo: 20")
+        self.assertEqual(str(self.maksukortti), "saldo: 0.2")
 
-    def saldo_vahenee_oikein_jos_rahaa_on_tarpeeksi(self):
+    def test_saldo_vahenee_oikein_jos_rahaa_on_tarpeeksi(self):
         self.maksukortti.ota_rahaa(5)
-        self.assertEqual(str(self.maksukortti), "saldo: 5")
+        self.assertEqual(str(self.maksukortti), "saldo: 0.05")
 
-    def saldo_ei_muutu_jos_rahaa_ei_ole_tarpeeksi(self):
+    def test_saldo_ei_muutu_jos_rahaa_ei_ole_tarpeeksi(self):
         self.maksukortti.ota_rahaa(15)
-        self.assertEqual(str(self.maksukortti), "saldo: 10")
+        self.assertEqual(str(self.maksukortti), "saldo: 0.1")
 
-    def ota_rahaa_palauttaa_True_jos_rahat_riittavat(self):
-        self.assertNotEqual(self.maksukortti.ota_rahaa(5), True)
+    def test_ota_rahaa_palauttaa_True_jos_rahat_riittavat(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(5), True)
 
-    def ota_rahaa_palauttaa_False_jos_rahat_eivat_riittavat(self):
-        self.assertNotEqual(self.maksukortti.ota_rahaa(15), False)
+    def test_ota_rahaa_palauttaa_False_jos_rahat_eivat_riittavat(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(15), False)
