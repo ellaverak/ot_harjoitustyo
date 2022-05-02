d992388ebec65a1ff96d8763ea2f4dbdd3f14ba5
ellaverak <ella.korkeaaho@gmail.com>
2022-05-02 18:27:22 +0300

    Add first version of pet_mommo

--- src/ui/mommo_view.py
+++ src/ui/mommo_view.py
@@ -1,4 +1,4 @@
-from tkinter import ttk, constants
+from tkinter import ttk, constants, StringVar
 from services.mommo_service import mommo_service
 from services.user_service import user_service
 
@@ -11,6 +11,9 @@ class MommoView:
         self.frame = None
         self.main_view = main_view
         self.all_mommos_view = all_mommos_view
+        self.pet_variable = None
+        self.pet_label = None
+        self.pet_message = None
 
         self.initialize()
 
@@ -33,6 +36,18 @@ class MommoView:
         mommo_service.water_mommo()
         self.initialize_mommo()
 
+    def pet_mommo(self):
+        self.pet_message = "Silitit mömmöä! :)"
+        self.pet_variable.set(self.pet_message)
+        self.pet_label.grid()
+
+        self.frame.after(2000, self.hide_pet_message)
+
+    def hide_pet_message(self):
+        self.pet_message = ""
+        self.pet_variable.set(self.pet_message)
+        self.pet_label.grid()
+
     def clean_mommo(self):
         mommo_service.clean_mommo()
         self.initialize_mommo()
@@ -52,7 +67,6 @@ class MommoView:
 
         self.frame.after(1000, self.initialize_mommo)
 
-
     def initialize(self):
         self.frame = ttk.Frame(master=self.root)
         mommo_label = ttk.Label(master=self.frame, text="Mömmöystävä")
@@ -61,6 +75,15 @@ class MommoView:
         mommo_thirst_label = ttk.Label(master=self.frame, text="Janoisuus:")
         mommo_clenliness_label = ttk.Label(master=self.frame, text="Puhtaus:")
         mommo_happiness_label = ttk.Label(master=self.frame, text="Onnellisuus:")
+        self.pet_variable = StringVar(self.frame)
+
+        self.pet_label = ttk.Label(
+            master=self.frame,
+            textvariable=self.pet_variable,
+            foreground='green'
+        )
+
+        self.pet_label.grid(row=1, column=2)
 
         self.initialize_mommo()
 
@@ -88,6 +111,12 @@ class MommoView:
             command=self.clean_mommo
         )
 
+        pet_button = ttk.Button(
+            master=self.frame,
+            text="Silitä",
+            command=self.pet_mommo
+        )
+
         all_mommos_button = ttk.Button(
             master=self.frame,
             text="Mömmöystävät",
@@ -106,4 +135,5 @@ class MommoView:
         feed_button.grid(row=2, column=2)
         water_button.grid(row=3, column=2)
         clean_button.grid(row=4, column=2)
+        pet_button.grid(row=5, column=2)
         all_mommos_button.grid(row=6, column=0)
